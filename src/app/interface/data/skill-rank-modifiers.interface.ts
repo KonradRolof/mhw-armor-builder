ead4c7a10fe758cdcdf80a29f9ca3e0457935bd9
Konrad Rolof <konrad.rolof@interlutions.de>
2019-10-18 16:51:50 +0200

    [FEATURE] Stats: Adds first modifier methods

--- src/app/interface/data/skill-rank-modifiers.interface.ts
+++ src/app/interface/data/skill-rank-modifiers.interface.ts
@@ -1,11 +1,11 @@
 export default interface SkillRankModifiers {
   affinity?: number; // Modifies the affinity of a weapon
   attack?: number; // Modifies the attack value of a weapon
-  damageFire?: number; // Modifies fire damage
-  damageWater?: number; // Modifies water damage
-  damageIce?: number; // Modifies ice damage
-  damageThunder?: number; // Modifies thunder damage
-  damageDragon?: number; // Modifies dragon damage
+  damageFire?: number|string; // Modifies fire damage
+  damageWater?: number|string; // Modifies water damage
+  damageIce?: number|string; // Modifies ice damage
+  damageThunder?: number|string; // Modifies thunder damage
+  damageDragon?: number|string; // Modifies dragon damage
   defense?: number; // Modifies the character's defense
   health?: number; // Modifies the character's maximum health
   sharpnessBonus?: number; // Modifies the maximum sharpness of a weapon
