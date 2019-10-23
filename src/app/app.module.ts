07e75571fec7ab40d252fcde63d74163476c1713
Konrad Rolof <konrad.rolof@interlutions.de>
2019-10-23 16:58:07 +0200

    [FEATURE] Saved sets: Adds button to delete a saved set

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { MhwCurrentSetComponent } from "./mhw-current-set/mhw-current-set.compon
 import { MhwSelectPopComponent } from "./mhw-select-pop/mhw-select-pop.component";
 import { HttpClientModule } from "@angular/common/http";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
+import { AngularSvgIconModule } from "angular-svg-icon";
 import { MhwStatsComponent } from "./mhw-stats/mhw-stats.component";
 import { MhwFooterComponent } from "./mhw-footer/mhw-footer.component";
 import { MhwSavedSetsComponent } from "./mhw-saved-sets/mhw-saved-sets.component";
@@ -29,7 +30,8 @@ import { SharpnessPxlPipe } from "./pipe/sharpness-pxl.pipe";
     AppRoutingModule,
     HttpClientModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    AngularSvgIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
